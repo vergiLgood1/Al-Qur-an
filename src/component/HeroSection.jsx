2444e17d139331327dfc9a586561aef74cec82ed
Diyo Anggara <xdamazon17@gmail.com>
2024-04-11 23:26:22 +0700

    tes

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,17 +1,17 @@
 import react from 'react';
 
 const Hero = () => {
-
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center ">
-                <div className="grid grid-cols-1">
-                    <h1 className="text-8xl font-bold">Ramadhan 1445 H</h1>
-                   
-                </div>
+        <div className="hero place-items-stretch min-h-screen bg-base-200 ">
+            <div className="flex flex-col text-center py-32">
+                <h1 className="text-8xl font-bold ">Ramadhan 1445 H</h1>
+            </div>
+            <div className=''>
+                <img src="/public/svg/mosque.svg" alt="" />
             </div>
         </div>
-    )
+    );
 }
+
 
 export default Hero;
